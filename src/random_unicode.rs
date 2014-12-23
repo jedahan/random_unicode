8fc2eb8130dc51fa894587af1c8f291b36ea2a7b
Jonathan Dahan <jonathan@jedahan.com>
2014-12-23 17:51:50 -0500

    print nothing for invalid characters

--- src/random_unicode.rs
+++ src/random_unicode.rs
@@ -1,11 +1,10 @@
 fn main() {
 
   for u in range(std::u32::MIN, std::char::MAX as u32) {
-    let character = match std::char::from_u32(u) {
-      Some(x) => x,
-      _ => ' '
+    match std::char::from_u32(u) {
+      Some(x) => print!("{}", x),
+      _ => print!("")
     };
-    print!("{}", character);
   }
 
 }
